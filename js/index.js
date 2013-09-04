e6f792e493792478b9b8006f44c6a2178560e7d4
GreatVV <marinegreatvv@yahoo.com>
2013-09-05 04:19:46 +0700

    Fix bugs with request function Add auth function with didn't work

--- js/index.js
+++ js/index.js
@@ -6,13 +6,45 @@ var exampleParams = [
 	},
 	{
 		name : "offset",
-		value : "5"
+		value : "0"
 	},
+	{
+		name : "fields",
+		value : "activity"
+	},
+	{
+		name : "count",
+		value : "100"
+	}
 ]
 
 
 $('#nearest').bind('pageinit', function(event) {
-    //loadEvents();
+	var exampleParams = [
+	{
+		name : "user_id",
+		value : "1112203"
+	},
+	{
+		name : "filter",
+		value : "events"
+	},
+	{
+		name : "fields",
+		value : "activity"
+	},
+	{
+		name : "count",
+		value : "100"
+	},
+	{
+		name : "extended",
+		value : "1"
+	}
+]
+// https://oauth.vk.com/blank.html#access_token=3c0dbad9ae2f522f9b4394fef543c2a7412910f76f0e3f2be924ba393fe1f64f5fcc463d970dc765319c9&expires_in=86400&user_id=1112203
+ //auth()
+   loadEvents( exampleParams,"3c0dbad9ae2f522f9b4394fef543c2a7412910f76f0e3f2be924ba393fe1f64f5fcc463d970dc765319c9");
 
     $("#allEvents").listview({
         autodividers: true,
@@ -23,10 +55,18 @@ $('#nearest').bind('pageinit', function(event) {
     }).listview("refresh");
 });
 
+function paramsToString(params) {
+	var paramString = "";
+	for (var i = 0; i<params.length; i++)
+	{
+		paramString += params[i]["name"]+"="+params[i]["value"] + "&";
+	}
+	return paramString;
+}
 
 function loadEvents(params, accessToken) {
 
-	$.ajax("https://api.vk.com/method/group.get?"+paramsToString(params)+"access_token=accessToken").done(function(data) {
+	$.ajax("https://api.vk.com/method/groups.get?"+paramsToString(params)+"access_token="+accessToken).done(function(data) {
      var i, userEvent;
      $.each(data.items, function (i, userEvent) {
      	//should show name
@@ -36,11 +76,8 @@ function loadEvents(params, accessToken) {
 });
 }
 
-function paramsToString(params) {
-	var paramString = "";
-	for (var i = 0; i<params.length; i++)
-	{
-		paramString = params["name"]+"="+params["value"] + "&";
-	}
-	return paramString;
+function auth() {
+	$.ajax("https://oauth.vk.com/authorize?client_id=3860770&redirect_uri=https://oauth.vk.com/blank.html&scope=groups&display=mobile&response_type=token").done(function(data) {
+		console.log(data)
+	})
 }
