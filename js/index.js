cb49f2dc6e6d0a620e26cb857989efa3892e2af3
GreatVV <marinegreatvv@yahoo.com>
2013-09-04 09:21:49 +0700

    Add mockup for loadEvents method (it can help Natasha to load application)

--- js/index.js
+++ js/index.js
@@ -1,4 +1,37 @@
+//it's example for params for function loadEvents
+var exampleParams = [
+	{
+		name : "user_id",
+		value : "6492"
+	},
+	{
+		name : "offset",
+		value : "5"
+	},
+]
+
 
 $('#nearest').bind('pageinit', function(event) {
-    
+    //loadEvents();
 });
+
+
+function loadEvents(params, accessToken) {
+
+	$.ajax("https://api.vk.com/method/group.get?"+paramsToString(params)+"access_token=accessToken").done(function(data) {
+     var i, userEvent;
+     $.each(data.items, function (i, userEvent) {
+     	//should show name
+        $("#allEvents").append("<p>"+userEvent.name+"</p>");
+     });
+});
+}
+
+function paramsToString(params) {
+	var paramString = "";
+	for (var i = 0; i<params.length; i++)
+	{
+		paramString = params["name"]+"="+params["value"] + "&";
+	}
+	return paramString;
+}
